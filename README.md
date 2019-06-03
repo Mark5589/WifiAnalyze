be36d1d32b2113252aa02e246a7eb59caab2793f
Mark Gurin <kaseopia.way@gmail.com>
2019-06-03 09:40:45 +0300

    Added: Upload to MonogoDB, Plotly, 90% done.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
+
+
 ## Installation
+
 ### Reqierments:
 ***NOTE*** its good to check that your machine has the following libraries (at least):  `pprint`
 
@@ -13,9 +16,9 @@
 
 2. make sure `mongoDB` python library is installed on your machine. the following import  `import pymongo` should be successful.
 
-   * also check that you can run MongoDB on your local machine cmd: `mongod`
+   * also check that you can run MongoDB on your local machine.	 cmd: `mongod`
 
-   * with any error try this 
+   * if  any error occurs try this 
 
      documentation: [http://api.mongodb.com/python/current/tutorial.html](http://api.mongodb.com/python/current/tutorial.html)
 
@@ -23,11 +26,11 @@
 
 ## Running
 
-To run this program use 
+To run this program use the following command:
 
 `python app.py <CSV file with wifi records> `
 
-the 3-th argument is some CSV file with wifi records that will be processed.
+the 3-th argument is the CSV file with wifi records that will be processed.
 
 
 
@@ -38,10 +41,26 @@ the 3-th argument is some CSV file with wifi records that will be processed.
 1. ~~make/create/build CSV file.~~
 2. ~~Read that file into the program~~
 3. ~~create JSON Object from wifi record.~~
-4. upload that JSON Object to MongoDB.atlas.WifiAnaylze.root.records
-5. represent chart of [ Security_Protocal: Amount ] // to do this we have to count the SecurityProtocol : amount relation
+4. ~~upload that JSON Object to MongoDB.atlas.WifiAnaylze.root.records~~
+5. ~~represent chart of [ Security_Protocal: Amount ] // to do this we have to count the SecurityProtocol : amount relation~~
 6. ~~Create some chart~~
 7. ~~make Connection with MongoDB Atlas~~
 8. ~~Try some connection to MongoDB, with uploading data to the db~~
-9. Upload our data to MongoDB Atlas
-10. Represent our data with Chart
+9. ~~Upload our data to MongoDB Atlas~~
+10. ~~Represent our data with Chart~~
+11. Avoid duplicates of the data. (With the help of the MAC)
+    * This fuctionalty shuold be done in the backend level because once the data is processed, it sends to the chart API and after that to the MongoDB
+
+
+
+## Images
+
+### plotly
+
+![chart](/Users/recon/workspace/3-python/1-wifiAnalyz/image/chart.png)
+
+### MongoDB Atlas
+
+
+
+![MongoAtlas](/Users/recon/workspace/3-python/1-wifiAnalyz/image/MongoAtlas.png)
--- src/app.py
+++ src/app.py
@@ -1,12 +1,27 @@
-import sys, pprint, re
+import sys, pprint, re, pymongo
 import plotly.plotly as py
 import plotly.graph_objs as go
+from pymongo import MongoClient
 
+# Array that will contain all records in JSON format
 FILE = []
-SECURITY_PROTOCOLS = []
+
+# Dictionary to represent the chart
+SECURITY_PROTOCOLS = {}
+
 
 CONST_COMMUNICATION = "WIFI\n"
 
+def securityProtocol_dict_add(arr):
+    for p in arr:
+        if p in SECURITY_PROTOCOLS:
+            SECURITY_PROTOCOLS[p] += 1
+        else:
+            SECURITY_PROTOCOLS[p] = 1
+
+#/ Reading the file, updating the 'FILE' Array with the Object 'obj',
+#  updating the Dictionary 'SECURITY_PROTOCOLS' with the security protocols.
+# /#
 def readFile(filePath):
     with open(filePath) as fp:
         for cnt, line in enumerate(fp):
@@ -15,7 +30,7 @@ def readFile(filePath):
             if cnt != 0 and cnt != 1 and currline[-1] == CONST_COMMUNICATION: 
                 enc_protocols_line = currline[2][1:-1]
                 arr_protocols = enc_protocols_line.split('][')
-
+                securityProtocol_dict_add(arr_protocols)
                 obj = {
                     'SSID': currline[1],
                     'MAC': currline[0],
@@ -23,35 +38,51 @@ def readFile(filePath):
                     'Channel': currline[4],
                     'FirstSeen': currline[3],
                 }
-                FILE.append(obj)
+                FILE.insert(0, obj)
+                
                 # print("{} ) line is : {}, LAST ELEMENT:: {}".format(cnt,currline, currline[-1]))
                 # print("obj:: {}".format(obj))
 
 def showDataChart():
     print('[!] Creating Chart ...Loading')
-
+    barX = []
+    barY = []
+    for key in SECURITY_PROTOCOLS:
+        barX.append(key)
+        barY.append(SECURITY_PROTOCOLS[key])
     data = [go.Bar(
-                x=['giraffes', 'orangutans', 'monkeys'],
-                y=[20, 14, 23]
+                x=barX,
+                y=barY
         )]
 
     py.iplot(data, filename='basic-bar')
 
+
+def sendDataToDB():
+    if FILE:
+        # client = pymongo.MongoClient("mongodb+srv://<USERNAME>:<PASSWORD>@cluster0-qnboi.mongodb.net/test?retryWrites=true&w=majority")
+       
+        db = client.WIFI
+        records = db.records
+        # db.records.create_index([('MAC', pymongo.ASCENDING)], unique=True)
+        records.insert_many(FILE)
+        print("[*] Uploaded, CHECK MongoDB")
+    else:
+        print("[! ERROR ] Canno't upload the data to MongoDB. ")
 
 if __name__ == '__main__' :
     print("[!] Reading File {}  ...Loading".format(sys.argv[1]))
     readFile(sys.argv[1])
     
     #DEBUG#
-    # pprint.pprint(FILE)
+    pprint.pprint(FILE)
     
     if FILE:
         print("[*] Complete Reading With Success!")
     else:
         print("[! ERROR ] ERROR, File Empty Or Bad Format")
     
-
     showDataChart()
+    sendDataToDB()
     # print(FILE)
-    
-    
+
